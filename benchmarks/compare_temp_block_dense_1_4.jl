d45e5e84d745f5cbdf0ce8d177ee2db88e958fc9
KonstantinosChatziantoniou <konstantic@ece.auth.gr>
2020-09-08 12:13:22 +0300

    changed var name. temporary block size increase

--- benchmarks/compare_temp_block_dense_1_4.jl
+++ benchmarks/compare_temp_block_dense_1_4.jl
@@ -3,15 +3,15 @@ include("../misc/misc.jl")
 include("../misc/cpu_stencils.jl")
 ## Star Stencil definition with radius = 4
 coefs = [0.5;0.25]
-star_stencil = @def_stencil_expression(
+dense_stencil = @def_stencil_expression(
             @sum(i,-1,1,
                 @sum(j,-1,1,
                     @sum(k,-1,1, c[max(abs(i),abs(j),abs(k))]*D[x+i,y+j,z+k]))))
-st_def = CreateStencilDefinition(star_stencil, coefs)
+st_def = CreateStencilDefinition(dense_stencil, coefs)
 st_inst1 = NewStencilInstance(st_def, m_step=false)
 st_inst2 = NewStencilInstance(st_def, m_step=2)
-st_inst3 = NewStencilInstance(st_def, m_step=3, bdim=16)
-st_inst4 = NewStencilInstance(st_def, m_step=4, bdim=16)
+st_inst3 = NewStencilInstance(st_def, m_step=3)
+st_inst4 = NewStencilInstance(st_def, m_step=4)
 
 ## Input Data size Definition
 radius = 4
