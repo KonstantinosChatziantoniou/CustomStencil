5941bd451be71f851293b7b4c9a067b1c1a49d60
KonstantinosChatziantoniou <konstantic@ece.auth.gr>
2020-09-13 15:47:37 +0300

    fx

--- tests/test_mgpu.jl
+++ tests/test_mgpu.jl
@@ -25,25 +25,25 @@ g1 = ApplyStencil(st_inst, data, t_steps)
 
 g2 = ApplyMultiGPU(2, st_inst, t_steps, data, t_group=2)
 
+#
+# using Plots
+@show (sum(abs.(g2 .- g1)))
+@show max(abs.(g2 .- g1)...)
+@show max(g2...)
 
-using Plots
-(sum(abs.(g2 .- g1)))
-max(abs.(g2 .- g1)...)
-max(g2...)
-
-
-ts = 2
-ind = size(g2,3)÷2 + ts
-heatmap(g2[:,:,ind] .- g1[:,:,ind])
-heatmap(g2[:,:,ind])
-heatmap(g1[:,:,ind])
-
-
-for i = 1:size(g1, 3)
-    er = sum(abs.(g1[:,:,i] .- g2[:,:,i]))
-    if er > 1
-        println("err ", i, " ", er )
-    end
-
-
-end
+#
+# ts = 2
+# ind = size(g2,3)÷2 + ts
+# heatmap(g2[:,:,ind] .- g1[:,:,ind])
+# heatmap(g2[:,:,ind])
+# heatmap(g1[:,:,ind])
+#
+#
+# for i = 1:size(g1, 3)
+#     er = sum(abs.(g1[:,:,i] .- g2[:,:,i]))
+#     if er > 1
+#         println("err ", i, " ", er )
+#     end
+#
+#
+# end
